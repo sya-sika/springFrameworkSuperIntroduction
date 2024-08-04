84c92cbbdb54b911f33535df68b12e5b6ef46a15
Sya-sika <formacambio@gmail.com>
2024-08-04 20:41:06 +0900

    chaper11 finished

--- quiz/src/main/java/com/example/quiz/repository/QuizRepository.java
+++ quiz/src/main/java/com/example/quiz/repository/QuizRepository.java
@@ -1,8 +1,10 @@
 package com.example.quiz.repository;
 
 import com.example.quiz.entity.Quiz;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuizRepository extends CrudRepository<Quiz, Integer> {
-
+    @Query("SELECT id FROM quiz ORDER BY RANDOM() limit 1")
+    Integer getRandomId();
 }
